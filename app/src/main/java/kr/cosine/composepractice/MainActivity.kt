5e69c84d6c9d5ee100f667f23c1a2221c2cc3351
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2023-12-19 18:02:09 +0900

    Initial commit

---
+++ app/src/main/java/kr/cosine/composepractice/MainActivity.kt
@@ -0,0 +1,168 @@
+package kr.cosine.composepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import kr.cosine.composepractice.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                Main()
+            }
+        }
+    }
+}
+
+@Composable
+fun Main() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) } // 변수 저장 (컴포저블 변경 시 초기화)
+    Surface {
+        if (shouldShowOnboarding) {
+            OnboardingScreen {
+                shouldShowOnboarding = false
+            }
+        } else {
+            Create(
+                "Java",
+                "Kotlin",
+                "Python",
+                "Go",
+                "Rust",
+                "Flutter",
+                "JavaScript",
+                "C",
+                "C++",
+                "C#"
+            )
+        }
+    }
+}
+
+@Composable
+fun Create(vararg names: String) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) } // 변수 저장 (컴포저블 변경되어도 초기화x)
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = "${name}은(는) 좋은 언어입니다. \n" +
+                            "아마도...?"
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.open)
+                } else {
+                    stringResource(R.string.close)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onButtonClicked: () -> Unit) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("어서오세요!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onButtonClicked
+        ) {
+            Text("계속하기")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
